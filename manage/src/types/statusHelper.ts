1fa150705d7e294c024e11814fbc7dd59e1785c0
羽羽羽 <16410704+feather-flurry@user.noreply.gitee.com>
2026-04-09 20:52:02 +0800

    1

--- manage/src/types/statusHelper.ts
+++
@@ -1,7 +0,0 @@
-import {statusTextMap} from '@/types/article'
-import type { ArticleStatus } from '@/types/article';
-//获取状态文字
-export const getStatusText=(statuss:ArticleStatus)=>{
-    console.log(statuss)
-    return statusTextMap[statuss]
-}
